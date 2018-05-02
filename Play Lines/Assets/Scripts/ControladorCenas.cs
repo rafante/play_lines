ebe95baa804f64c4126ac37565cc8ed67b708e14
rafante2 <rafante2@gmail.com>
2018-05-01 21:41:00 -0300

    Protótipo da tela de menu e de layout da história

---
+++ Play Lines/Assets/Scripts/ControladorCenas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorCenas : MonoBehaviour {
+
+	public void carregarCena(string nomeDaCena){
+		SceneManager.LoadScene (nomeDaCena);
+	}
+}
